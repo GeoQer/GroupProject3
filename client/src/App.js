362f82279fd2fa4a5ef7ffc60fe15fa6cbee651a
Kevin Davis <dawnpatrol506@gmail.com>
2018-12-22 23:35:33 -0700

    initial setup and deployment

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class App extends Component {
+
+  render() {
+    return (
+      <div>
+        <h1>Project 3</h1>
+        <p>It doesn't look like much, but the boilerplate is ready and the project is easy to deploy... Let's do this thing.</p>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ functions/index.js
@@ -0,0 +1,22 @@
+const functions = require('firebase-functions');
+const express = require("express");
+const path = require("path");
+const PORT = process.env.PORT || 8080;
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}!`);
+});
+
+exports.app = functions.https.onRequest(app);
