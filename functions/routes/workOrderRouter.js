3c4e52c085b73a231661fbde5908bbd0fa5257ea
Kevin Davis <dawnpatrol506@gmail.com>
2018-12-28 12:47:08 -0700

    work orders routes working

--- functions/routes/workOrderRouter.js
+++ functions/routes/workOrderRouter.js
@@ -7,10 +7,51 @@ router.get('/all', (req, res) => {
         .then(docs => {
             let arr = [];
             docs.forEach(doc => arr.push(doc.data()));
-            res.json({arr});
+            res.json(arr);
         })
-        .catch(err => res.json({ err });
+        .catch(err => res.json({ err }));
 })
+
+router.get('/active', (req, res) => {
+    db.collection('work-orders').where('isComplete', '==', false).get()
+        .then(docs => {
+            const arr = [];
+            docs.forEach(doc => arr.push({...doc.data(), id: doc.id}));
+            res.json(arr);
+        })
+        .catch(err => res.json({ err }))
+})
+
+router.get('/complete', (req, res) => {
+    db.collection('work-orders').where('isComplete', '==', true).get()
+        .then(docs => {
+            const arr = [];
+            docs.forEach(doc => arr.push({...doc.data(), id: doc.id}));
+            res.json(arr);
+        })
+        .catch(err => res.json({ err }))
+})
+
+router.put('/update', (req, res) => {
+    const Props = JSON.parse(req.body.props);
+    db.collection('work-orders').doc(req.body.id).set({...newProps}, {merge: true})
+        .then(() => res.json({success: true}))
+        .catch(err => res.json({ err }));
+});
+
+router.delete('/:id', (req, res) => {
+    db.collection('work-orders').doc(req.params.id).delete()
+        .then(() => res.json({success: true}))
+        .catch(err => res.json({ err }));
+})
+
+router.post('/create', (req, res) => {
+    const props = JSON.parse(req.body.props);
+    db.collection('work-orders').add({...props})
+        .then(doc => res.json({id: doc.id}))
+        .catch(err => res.json({ err }));
+})
+
 
 
 module.exports = router;
