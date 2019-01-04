2a11f6ce339320911228c30bf0b604188f3011c3
Kevin Davis <dawnpatrol506@gmail.com>
2019-01-03 20:55:55 -0700

    you know... stuff

--- client/src/components/employee.js
+++ client/src/components/employee.js
@@ -1,13 +1,13 @@
 import React from "react";
 import MenuBar from "./workStations/menu-bar/index";
-import ColdSaw from "./workStations/pages/coldsaw";
-import Grinding from "./workStations/pages/grinding";
-import Laser from "./workStations/pages/laser";
-import PressBrake from "./workStations/pages/pressBrake";
-import Programming from "./workStations/pages/programming";
-import TubeBender from "./workStations/pages/tubeBender";
-import Welding from "./workStations/pages/welding";
-import { Route } from 'react-router-dom';
+// import ColdSaw from "./workStations/pages/coldsaw";
+// import Grinding from "./workStations/pages/grinding";
+// import Laser from "./workStations/pages/laser";
+// import PressBrake from "./workStations/pages/pressBrake";
+// import Programming from "./workStations/pages/programming";
+// import TubeBender from "./workStations/pages/tubeBender";
+// import Welding from "./workStations/pages/welding";
+// import { Route } from 'react-router-dom';
 import Axios from 'axios';
 import Card from '../components/Card';
 
@@ -21,31 +21,23 @@ class Employee extends React.Component {
     }
 
     Axios.get('/api/v1/stations/all')
-      .then(result => this.setState({stations: result.data}));
+      .then(result => this.setState({ stations: result.data }));
   }
 
   handleStationSelect = (event) => {
     Axios.get(`/api/v1/workorders/active/${event.target.getAttribute('data-id')}`)
-      .then(result => this.setState({workOrders: result.data}));
+      .then(result => this.setState({ workOrders: result.data }));
   }
 
   render() {
     return (
       <div>
-        <MenuBar stations={this.state.stations} handleStationSelect={this.handleStationSelect}/>
+        <MenuBar stations={this.state.stations} handleStationSelect={this.handleStationSelect} />
         {this.state.workOrders.map(workOrder => {
-          return(
+          return (
             <Card text={workOrder.text} title="this is a work order" />
           )
         })}
-        
-        {/* <Route exact path={`/employee/programming`} component={Programming} />
-        <Route exact path={`/employee/coldsaw`} component={ColdSaw} />
-        <Route exact path={`/employee/grinding`} component={Grinding} />
-        <Route exact path={`/employee/laser`} component={Laser} />
-        <Route exact path={`/employee/pressbrake`} component={PressBrake} />
-        <Route exact path={`/employee/tubebender`} component={TubeBender} />
-        <Route exact path={`/employee/welding`} component={Welding} /> */}
       </div>
     );
   }
