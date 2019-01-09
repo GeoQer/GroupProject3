ed92876e1c578e6dde0a76fdf13f19ebae348291
Kevin Davis <dawnpatrol506@gmail.com>
2019-01-09 09:19:57 -0700

    firebase compatibility changes

---
+++ client/build/precache-manifest.2ccfce2961ef180386753fa5779f711f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e7f84f71468086dd93d9",
+    "url": "/static/js/main.e7f84f71.chunk.js"
+  },
+  {
+    "revision": "78928d380e2b690a1564",
+    "url": "/static/js/1.78928d38.chunk.js"
+  },
+  {
+    "revision": "e7f84f71468086dd93d9",
+    "url": "/static/css/main.563b5f84.chunk.css"
+  },
+  {
+    "revision": "fd8dc21d7a3e011af624109881804a09",
+    "url": "/index.html"
+  }
+];
