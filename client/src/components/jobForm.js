4385482b64bda241447691429ff56f907ec066ff
GeoQer <40548603+GeoQer@users.noreply.github.com>
2019-01-16 17:02:43 -0700

    modified jobForm and jobPage. Still need work.

--- client/src/components/jobForm.js
+++ client/src/components/jobForm.js
@@ -4,10 +4,10 @@ import { Modal, FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
 const firebase = require('firebase/app');
 require('firebase/storage');
 
-const Station = props => (
+const Part = props => (
     <div>
-        <h2><span className="label label-primary" style={{ float: "left", marginRight: "7px" }}>{props.stationName}</span></h2>
-        <button data-id={props.id} className="btn btn-danger" onClick={props.removeStation}>x</button>
+        <h2><span className="label label-primary" style={{ float: "left", marginRight: "7px" }}>{props.partName}</span></h2>
+        <button data-id={props.id} className="btn btn-danger" onClick={props.removePart}>x</button>
     </div>
 )
 
@@ -15,26 +15,26 @@ class JobForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            selectedStation: {},
-            stations: [],
-            part: {
+            selectedPart: {},
+            parts: [],
+            job: {
                 id: '',
-                doc: '',
-                stations: []
+                quantity: '',
+                parts: []
             },
             showModal: false
         }
-        Axios.get('/api/v1/stations/all')
-            .then(result => this.setState({ stations: result.data, selectedStation: result.data[0] }));
+        Axios.get('/api/v1/work-orders/all')
+            .then(result => this.setState({ parts: result.data, selectedPart: result.data[0] }));
     };
 
-    removeStation = event => {
+    removePart = event => {
         event.preventDefault();
         const id = event.target.getAttribute('data-id');
-        const obj = Object.assign(this.state.part);
-        const arr = this.state.part.stations.slice(0);
-        arr.forEach((station, index) => {
-            if (station.id === id) {
+        const obj = Object.assign(this.state.job);
+        const arr = this.state.job.parts.slice(0);
+        arr.forEach((part, index) => {
+            if (part.id === id) {
                 arr.splice(index, 1);
             }
         })
@@ -49,29 +49,29 @@ class JobForm extends React.Component {
     handleSelectChange = (event) => {
         const id = event.target.value;
         const name = event.target.children[event.target.selectedIndex].text;
-        this.setState({ selectedStation: { id, name } });
+        this.setState({ selectedPart: { id, name } });
     }
 
     addStation = () => {
-        const obj = Object.assign(this.state.part);
-        obj.stations.push(this.state.selectedStation);
-        this.setState({ part: obj, showModal: false, selectedStation: this.state.stations[0] });
+        const obj = Object.assign(this.state.job);
+        obj.stations.push(this.state.selectedPart);
+        this.setState({ part: obj, showModal: false, selectedJob: this.state.parts[0] });
     }
 
     handleInput = (event) => {
         const name = event.target.name;
-        let obj = Object.assign(this.state.part);
+        let obj = Object.assign(this.state.job);
         obj[name] = event.target.value;
-        this.setState({ part: obj });
+        this.setState({ job: obj });
     }
 
     clear = () => {
-        document.getElementById('part-id').value = '';
-        this.setState({ part: { id: '', stations: [] } });
+        document.getElementById('job-id').value = '';
+        this.setState({ part: { id: '', parts: [] } });
     }
 
     handleSubmit = async () => {
-        if(this.state.part.stations.length < 1){
+        if(this.state.job.parts.length < 1){
             console.log('There is no information to post');
             return;
         }
@@ -84,8 +84,8 @@ class JobForm extends React.Component {
             reader.onloadend = (e) => {
                 const blob = new Blob([e.target.result], { type: file.type });
 
-                Axios.post('/api/v1/parts/create', {
-                    part: {...this.state.part, filename: file.name}
+                Axios.post('/api/v1/work-orders/create', {
+                    job: {...this.state.part, filename: file.name}
                 })
                 .then(response => {
                     const ref = firebase.storage().ref(`/${response.data.newPartID}/${file.name}`);
@@ -96,8 +96,8 @@ class JobForm extends React.Component {
             reader.readAsArrayBuffer(file);
         }
         else{
-            Axios.post('/api/v1/parts/create', {
-                part: {...this.state.part, filename: 'no file'}
+            Axios.post('/api/v1/work-orders/create', {
+                job: {...this.state.job, filename: 'no file'}
             })
             .catch(err => console.log(err));
         }
@@ -106,15 +106,15 @@ class JobForm extends React.Component {
     render() {
         return (
             <div className="container">
-                <form id="part-form" action="/api/v1/parts/test" method="POST">
+                <form id="job-form" action="/api/v1/work-orders/test" method="POST">
                     <div className="input-group">
-                        <span className="input-group-addon" id="part-number-addon">Part ID</span>
-                        <input name="id" id="part-id" onChange={this.handleInput} type="text" className="form-control" placeholder="If left blank an ID will be auto-generated" aria-describedby="part-number-addon" />
+                        <span className="input-group-addon" id="job-number-addon">Job ID</span>
+                        <input name="id" id="job-id" onChange={this.handleInput} type="text" className="form-control" placeholder="If left blank an ID will be auto-generated" aria-describedby="job-number-addon" />
                     </div>
                     <div className="input-group">
-                        <input name="doc" id="attachment" onChange={this.handleInput} type="file" className="form-control" aria-describedby="part-document-addon" />
+                        <input name="doc" id="attachment" onChange={this.handleInput} type="file" className="form-control" aria-describedby="job-document-addon" />
                     </div>
-                    {this.state.part.stations.map((station, index) => <Station key={index} stationName={station.name} id={station.id} removeStation={this.removeStation} />)}
+                    {this.state.job.parts.map((part, index) => <Part key={index} partName={part.name} id={part.id} removePart={this.removePart} />)}
                 </form>
                 <br />
                 <button className="btn btn-success" onClick={this.showModal}>Add Station</button>
@@ -125,15 +125,15 @@ class JobForm extends React.Component {
                     <button className="btn btn-danger" onClick={this.clear}>Clear</button>
                 </div>
                 <Modal show={this.state.showModal}>
-                    <Modal.Title>Select a Station</Modal.Title>
+                    <Modal.Title>Select a Part</Modal.Title>
                     <FormGroup>
-                        <ControlLabel>Stations</ControlLabel>
-                        <FormControl componentClass="select" placeholder="Select a station" onChange={this.handleSelectChange}>
-                            {this.state.stations.map(station => <option key={station.id} value={station.id}>{station.name}</option>)}
+                        <ControlLabel>Parts</ControlLabel>
+                        <FormControl componentClass="select" placeholder="Select a part" onChange={this.handleSelectChange}>
+                            {this.state.parts.map(part => <option key={part.id} value={part.id}>{part.name}</option>)}
                         </FormControl>
                     </FormGroup>
                     <FormGroup>
-                        <button className="btn btn-success" onClick={this.addStation}>OK</button>
+                        <button className="btn btn-success" onClick={this.addPart}>OK</button>
                         <button className="btn btn-danger" onClick={this.hideModal}>Cancel</button>
                     </FormGroup>
                 </Modal>
---
+++ client/src/components/jobsPage.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import JobForm from './jobForm';
+import { Route, Link } from 'react-router-dom';
+import Axios from 'axios';
+const firebase = require('firebase/app');
+require('firebase/storage');
+require('firebase/firestore');
+var config = {
+    apiKey: "AIzaSyAZB-qbjpKVRvaQt17kPsPTMav3O12by6k",
+    authDomain: "project-runner-f1bdc.firebaseapp.com",
+    databaseURL: "https://project-runner-f1bdc.firebaseio.com",
+    projectId: "project-runner-f1bdc",
+    storageBucket: "project-runner-f1bdc.appspot.com",
+    messagingSenderId: "757776283780"
+};
+firebase.initializeApp(config);
+
+const ViewJobs = props => (
+    <div className="row">
+        {props.jobs.map(part => <JobCard key={job.id} title={job.id} parts={job.parts} filepath={job.filepath} viewAttachment={props.viewAttachment} />)}
+    </div>
+)
+
+const JobCard = props => (
+    <div className="col-sm-6 col-md-4">
+        <div className="thumbnail" >
+            <div className="caption">
+                <h3 className="card-title">{props.title}</h3>
+                <p><strong>Parts: </strong></p>
+                {props.parts.map(part => <p key={part.id}>{part.name}</p>)}
+                <button className="btn btn-primary" data-filepath={props.filepath} onClick={props.viewAttachment}>View Attachment</button>
+            </div>
+        </div>
+    </div>
+);
+
+class JobPage extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            Jobs: [],
+            interval: 0
+        }
+
+    }
+
+    componentDidMount = () => {
+        Axios.get('/api/v1/work-orders/all')
+            .then(result => this.setState({jobs: result.data}));
+    
+        let x = setInterval(() => {
+            Axios.get('/api/v1/work-orders/all')
+            .then(result => this.setState({jobs: result.data}));
+        }, 15000);
+        this.setState({interval: x});    
+    }
+
+    componentWillUnmount = () => {
+        clearInterval(this.state.interval);
+    }
+
+    handleTabSelect = (event) => {
+        const target = event.target;
+        const tabs = document.getElementsByClassName('tab-link');
+        for (let i = 0; i < tabs.length; i++) {
+            tabs[i].setAttribute('class', 'tab-link');
+        }
+        target.parentElement.setAttribute('class', 'active tab-link');
+    }
+
+    viewAttachment = event => {
+        firebase.storage().ref(event.target.getAttribute('data-filepath')).getDownloadURL()
+            .then(url => window.open(url, '_blank'));
+    }
+
+    render = props => (
+        <div className="container">
+            <ul className="nav nav-pills">
+                <li role="presentation" className="active tab-link" onClick={this.handleTabSelect}><Link to="/admin/work-orders/view">View</Link></li>
+                <li role="presentation" className="tab-link" onClick={this.handleTabSelect}><Link to="/admin/work-orders/edit">Edit</Link></li>
+                <li role="presentation" className="tab-link" onClick={this.handleTabSelect}><Link to="/admin/work-orders/create">Create</Link></li>
+            </ul>
+            <br />
+            <br />
+            <Route path="/admin/work-orders/create" component={JobForm} />
+            <Route path="/admin/work-order/view" component={() => <ViewJobs jobs={this.state.jobs} viewAttachment={this.viewAttachment} />} />
+        </div>
+    )
+}
+export default JobPage;
